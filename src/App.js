d74b2d92b583cbccb87a696b964023f0ce2dfaca
Nikhil Aggarwal <nikhil0929@gmail.com>
2022-04-03 17:02:09 -0500

    Added fetch data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const schedule = {
     "title": "CS Courses for 2021-2022",
@@ -57,11 +57,30 @@ const CourseList = ({ courses }) => (
     </div>
 );
 
-const App = () =>  (
-    <div className="container">
-        <Banner title={ schedule.title } />
-        <CourseList courses={ schedule.courses } />
-    </div>
-);
+const App = () => {
+
+    const [schedule, setSchedule] = useState()
+
+    const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+    useEffect(() => {
+        const fetchSchedule = async () => {
+            const response = await fetch(url);
+            if (!response.ok) throw response;
+            const json = await response.json();
+            setSchedule(json);
+        }
+        fetchSchedule();
+    }, []);
+
+    if (!schedule) return <h1>Loading schedule...</h1>
+
+    return (
+        <div className="container">
+            <Banner title={schedule.title}/>
+            <CourseList courses={schedule.courses}/>
+        </div>
+    )
+};
 
 export default App;
