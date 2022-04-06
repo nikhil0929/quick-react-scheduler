c5f48d1d0448a9c8b9e73de5b667e2fe03a3ed3a
Nikhil Aggarwal <nikhil0929@gmail.com>
2022-04-06 18:59:36 -0500

    Modularized codebase

---
+++ src/components/Course.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {getCourseNumber, getCourseTerm, hasConflict} from "../utilities/times";
+
+const toggle = (x, lst) => (
+    lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
+);
+
+function Course({course, selected, setSelected}) {
+    const isSelected = selected.includes(course);
+    const isDisabled = !isSelected && hasConflict(course, selected);
+    const style = {
+        backgroundColor: isDisabled ? 'lightgrey' : isSelected ? 'lightgreen' : 'white'
+    };
+    return (
+        <div className="card m-1 p-2"
+             style={style}
+             onClick={isDisabled ? null : () => setSelected(toggle(course, selected))}>
+            <div className="card-body">
+                <div className="card-title">{getCourseTerm(course)} CS {getCourseNumber(course)}</div>
+                <div className="card-text">{course.title}</div>
+                <div className="card-text">{course.meets}</div>
+            </div>
+        </div>
+    );
+}
+
+export default Course
